09a2e31b876daa486954322fa3f0461a929b9b23
Devin Polichetti <devinpolichetti@gmail.com>
2023-04-03 10:34:32 -0400

    added server

---
+++ server.js
@@ -0,0 +1,51 @@
+// dependencies
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+
+// set up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// set up sessions
+const sess = {
+  secret: "secret",
+  // set to expire after 10 minutes of idle time
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers"));
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  sequelize.sync({ force: false });
+});
